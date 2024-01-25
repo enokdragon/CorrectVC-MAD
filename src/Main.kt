8e65567f5d8ee3cb8d778c522b304d4052ea26be
Enokd <edragon851@students.mchenry.edu>
2024-01-25 11:15:34 -0600

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,9 @@
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("My name is Enok.")
+        println("I've been coding for two years.")
+        println("I've never worked with mobile apps.")
+        println("But I am excited to learn.")
+    }
+}
